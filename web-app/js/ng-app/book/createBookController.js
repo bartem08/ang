62d228cc6592b23b5d7f768989fbbea58c0531a6
artem08 <artem2549@gmail.com>
2017-12-08 00:18:25 +0200

    Add book validation, search

--- web-app/js/ng-app/book/createBookController.js
+++ web-app/js/ng-app/book/createBookController.js
@@ -11,5 +11,24 @@ function createBookController($scope, $location, bookService) {
         bookService.createBook($scope).then(function () {
             $location.path('book/list');
         });
+    };
+
+    $scope.isNameValid = function () {
+        var name = $scope.name;
+        return name && name.length >= 3
+    };
+
+    $scope.isGenreValid = function () {
+        var genre = $scope.genre;
+        return genre && genre.length >= 3
+    };
+
+    $scope.isYearValid = function () {
+        var year = $scope.year;
+        return year && year >= 1000 && year <= (new Date()).getFullYear()
+    };
+
+    $scope.isAuthorValid = function () {
+        return $scope.authorId;
     }
 }
