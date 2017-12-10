f855566fbc99afd6603303dab2a1089a0d83763c
artem08 <artem2549@gmail.com>
2017-12-10 22:13:19 +0200

    Use form validation directives

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -41,6 +41,7 @@ modules = {
         resource url: '/js/ng-app/book/bookController.js'
         resource url: '/js/ng-app/book/createBookController.js'
         resource url: '/js/ng-app/book/bookDetailsController.js'
+        resource url: '/js/ng-app/validators/pastValidator.js'
     }
 
     author {
--- web-app/js/ng-app/book/createBookController.js
+++ web-app/js/ng-app/book/createBookController.js
@@ -12,23 +12,4 @@ function createBookController($scope, $location, bookService) {
             $location.path('book/list');
         });
     };
-
-    $scope.isNameValid = function () {
-        var name = $scope.name;
-        return name && name.length >= 3
-    };
-
-    $scope.isGenreValid = function () {
-        var genre = $scope.genre;
-        return genre && genre.length >= 3
-    };
-
-    $scope.isYearValid = function () {
-        var year = $scope.year;
-        return year && year >= 1000 && year <= (new Date()).getFullYear()
-    };
-
-    $scope.isAuthorValid = function () {
-        return $scope.authorId;
-    }
 }
